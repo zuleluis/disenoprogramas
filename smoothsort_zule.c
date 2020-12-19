c4af8e23c3bcbabd604ce73ea5ffdff3f705a6e8
zuleluis <64336166+zuleluis@users.noreply.github.com>
2020-12-19 03:02:32 -0600

    Update smoothsort_zule.c

--- smoothsort_zule.c
+++ smoothsort_zule.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef const char *palabra;
+typedef char *palabra;
 
 struct estado{
 	palabra *cadena;
@@ -20,25 +20,18 @@ void semitrinkle(struct estado *elemento);
 void smoothsort(palabra *a, unsigned int longitud);
 
 int main(int argc, const char **argv) {
-	const char *lista[] = {
-        "the", 
-		"quick", 
-		"brown", 
-		"fox", 
-		"jumps", 
-		"over", 
-		"the", 
-		"lazy", 
-		"dog"
-        /*"ABV","Agustión","Bravo Váquez","Oftalmología","CGG","Caridad","García García","Intensivos", "FMA","Federico","Moreno Álvarez","Análisis",
-        "ARG","Inmaculada","Ruiz Guillén","Radiología", "JBH","Juan","Belver Hernández","Psiquiatría", "JFF","Joaquín","Fernández Fernández","Ginecología",
-        "JPRG","Juan Pedro","Rubio García","Pediatría","LFG","Laura","Fernández Guillén","General", "MGM","Mónica","Gutiérrez Martínez","Intensivos",
-        "MHA","Alejandro","Meroño Hernández","Pediatría", "OAM","Olga","Atienza Martínez","Cirugía"*/
+	char *lista[] = {
+        "perro", "gato", "oso"/*, "leon", "tigre", "lemur", "caballo"/*,
+		"ardilla", "elefante", "suricata", "jirafa", "cebra", "lechuza",
+		"delfin", "tiburon", "hiena", "zorro", "buho", "foca", "iguana", 
+		"koala", "mariposa", "nutria", "quokka", "rinoceronte", "urraca",
+		"venado", "xoloitzcuintle", "yegua"*/
 	};
     int longitud=sizeof(lista)/sizeof(lista[0]);
 	unsigned int i;
 
 	smoothsort(lista, longitud);
+	printf("\n-----------------------------\n");
 
     printf("La lista contiene %i elementos\n", longitud);
     printf("---------------------------------\n");
@@ -57,11 +50,11 @@ void smoothsort(palabra *palabra, unsigned int longitud){
 	unsigned int temporal;
 
 	elemento.cadena = palabra;
+	printf("\n----SMOOTHSORT-----\n");
     printf("elemento.cadena -> %s\n", *palabra);
 	elemento.r = 0;
 	elemento.p = elemento.b = elemento.c = 1;
 
-	/* Build tree */
 	for (elemento.q = 1; elemento.q < longitud; elemento.q++) {
 		elemento.r1 = elemento.r;
 		if ((elemento.p & 7) == 3){
@@ -71,7 +64,6 @@ void smoothsort(palabra *palabra, unsigned int longitud){
 
 			elemento.p = (elemento.p + 1) >> 2;
 
-			/* Two "up"s, saves us a little time */
 			temporal = elemento.b + elemento.c + 1;
 			elemento.b = elemento.b + temporal + 1;
 			elemento.c = temporal;
@@ -93,10 +85,8 @@ void smoothsort(palabra *palabra, unsigned int longitud){
 
 			elemento.p++;
 		}
-
 		elemento.r++;
 	}
-
 	elemento.r1 = elemento.r;
 	trinkle(&elemento);
 
@@ -129,8 +119,8 @@ void smoothsort(palabra *palabra, unsigned int longitud){
 	}
 }
 
-//Si comprobamos que b>a, entonces *b se intercambia con *a, pasa a la posicion de b y aumenta 1
 void sube(unsigned int *a, unsigned int *b){
+	printf("Subiendo...\n");
 	unsigned int temporal;
 
 	temporal = *a; 
@@ -139,6 +129,7 @@ void sube(unsigned int *a, unsigned int *b){
 }
 
 void baja(unsigned int *a, unsigned int *b){
+	printf("Bajando...\n");
 	unsigned int temporal;
 
 	temporal = *b;
@@ -149,24 +140,31 @@ void baja(unsigned int *a, unsigned int *b){
 void sift(struct estado *elemento){
 	unsigned int r0, r2;
 	palabra temporal;
+	printf("\n------------SIFT--------------------\n");
 
 	r0 = elemento->r1;
 	temporal = elemento->cadena[r0];
+	printf("temporal=elemento->cadena[r0] -> %s\n", temporal);
 
 	while (elemento->b1 > 2) {
 		r2 = elemento->r1 - elemento->b1 + elemento->c1;
 
 		if (compara(elemento->cadena[elemento->r1 - 1], elemento->cadena[r2]) >= 0) {
+			printf("elemento->cadena[elemento->r1 -1] -> %s\n", elemento->cadena[elemento->r1-1]);
+			printf("elemento->cadena[elemento->r2] -> %s\n", elemento->cadena[r2]);
 			r2 = elemento->r1 - 1;
 
 			baja(&elemento->b1, &elemento->c1);
 		}
 
 		if (compara(elemento->cadena[r2], temporal) < 0) {
+			printf("elemento->cadena[r2] -> %s\n", elemento->cadena[r2]);
+			printf("temporal -> %s\n", temporal);
 			elemento->b1 = 1;
 		}
         else {
 			elemento->cadena[elemento->r1] = elemento->cadena[r2];
+			printf("elemento->cadena[elemento->r1] -> %s\n", elemento->cadena[elemento->r1]);
 			elemento->r1 = r2;
 
 			baja(&elemento->b1, &elemento->c1);
@@ -175,6 +173,7 @@ void sift(struct estado *elemento){
 
 	if (elemento->r1 - r0 > 0) {
 		elemento->cadena[elemento->r1] = temporal;
+		printf("elemento->cadena[elemento->r1] -> %s\n", elemento->cadena[elemento->r1]);
 	}
 }
 
@@ -187,6 +186,8 @@ void trinkle(struct estado *elemento){
 	elemento->c1 = elemento->c;
 	r0 = elemento->r1; 
 	temporal = elemento->cadena[r0];
+	printf("\n------------TRINKLE--------------------\n");
+	printf("temporal -> %s\n", temporal);
 
 	while (p1 > 0) {
 		while((p1 & 1) == 0) {
@@ -197,33 +198,37 @@ void trinkle(struct estado *elemento){
 
 		r3 = elemento->r1 - elemento->b1;
 
-		if(p1 == 1 || compara(elemento->cadena[r3], temporal) < 0){
-			p1 = 0;
-		} 
+		if(p1 == 1 || compara(elemento->cadena[r3], temporal) < 0) p1 = 0;		
         else{
 			p1--;
 
 			if (elemento->b1 == 1){
 				elemento->cadena[elemento->r1] = elemento->cadena[r3];
+				printf("elemento->cadena[elemento->r1] -> %s\n", elemento->cadena[elemento->r1]);
 				elemento->r1 = r3;
 			} 
             else if (elemento->b1 >= 3){
 				r2 = elemento->r1 - elemento->b1 + elemento->c1;
 
 				if(compara(elemento->cadena[elemento->r1 - 1], elemento->cadena[r2]) >= 0){
+					printf("elemento->cadena[elemento->r1-1] -> %s\n", elemento->cadena[elemento->r1-1]);
+					printf("elemento->cadena[r2] -> %s\n", elemento->cadena[r2]);
 					r2 = elemento->r1 - 1;
 
 				    baja(&elemento->b1, &elemento->c1);
 
-					p1 <<= 1;
+					p1 = p1 << 1;
 				}
 
 				if(compara(elemento->cadena[r2], elemento->cadena[r3]) < 0) {
 					elemento->cadena[elemento->r1] = elemento->cadena[r3];
+					printf("elemento->cadena[r2] -> %s\n", elemento->cadena[r2]);
+					printf("elemento->cadena[r3] -> %s\n", elemento->cadena[r3]);
 					elemento->r1 = r3;
 				}
                 else {
 					elemento->cadena[elemento->r1] = elemento->cadena[r2];
+					printf("elemento->cadena[elemento->r1] -> %s\n", elemento->cadena[elemento->r1]);
 					elemento->r1 = r2;
 
 					baja(&elemento->b1, &elemento->c1);
@@ -236,6 +241,7 @@ void trinkle(struct estado *elemento){
 
 	if (elemento->r1 - r0 != 0) {
 		elemento->cadena[elemento->r1] = temporal;
+		printf("elemento->cadena[elemento->r1] -> %s\n", elemento->cadena[elemento->r1]);
 	}
 
 	sift(elemento);
@@ -243,13 +249,17 @@ void trinkle(struct estado *elemento){
 
 void semitrinkle(struct estado *elemento){
 	palabra temporal;
+	printf("\n--------------SEMITRINKLE-----------\n");
 
 	elemento->r1 = elemento->r - elemento->c;
 
 	if (compara(elemento->cadena[elemento->r1], elemento->cadena[elemento->r]) >= 0) {
 		temporal = elemento->cadena[elemento->r];
+		printf("temporal -> %s\n", temporal);
 		elemento->cadena[elemento->r] = elemento->cadena[elemento->r1];
+		printf("elemento->cadena[elemento->r] -> %s\n", elemento->cadena[elemento->r]);
 		elemento->cadena[elemento->r1] = temporal;
+		printf("elemento->cadena[elemento->r1] -> %s\n", elemento->cadena[elemento->r1]);
 
 		trinkle(elemento);
 	}
